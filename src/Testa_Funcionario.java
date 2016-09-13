f343ba9ad6cc39c5f0d977e01ff7705ad624510c
fabricio-2Uu <2uumobile@gmail.com>
2016-09-13 20:44:53 -0300

    Começando o remoto.
    Incluindo os arquivos inciais.

---
+++ src/Testa_Funcionario.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Testa_Funcionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Digite o Nome: ");
+		String nome = keyboard.nextLine();
+		
+		System.out.println("Digite o Sexo (M ou F): ");
+		char sexo = keyboard.nextLine().charAt(0);
+		
+		System.out.println("Digite o CPF: ");
+		String cpf = keyboard.nextLine();
+		
+		System.out.println("Digite o Salário: ");
+		double salario = keyboard.nextDouble();
+		keyboard.nextLine();
+		
+		System.out.println("Digite o Endereço: ");
+		String rua = keyboard.nextLine();
+		
+		System.out.println("Digite o Número: ");
+		int numero = keyboard.nextInt();
+		
+		keyboard.close();
+		
+		Endereco e = new Endereco(rua, numero);
+		Funcionario Func1 = new Funcionario(nome, sexo, cpf, salario, e);
+		Funcionario Func2 = new Funcionario("Jorge", 'M', "654.456.369-95", 3214.32, e);
+		Funcionario Func3 = Func2;
+		System.out.println(Func1.equals(Func2));
+		System.out.println(Func1.equals(Func3));
+		System.out.println(Func2.equals(Func3));
+		System.out.println();
+		
+	}
+
+}
